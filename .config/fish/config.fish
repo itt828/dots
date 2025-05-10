e40d0c531e411e2d2a8c8e3e70f0757c67195988
itt828 <iittt828@gmail.com>
2025-05-11 00:15:46 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,22 @@
 if status is-interactive
-    starship init fish | source
-    fish_add_path $HOME/.dotnet/tools
-    abbr -a -- ls eza --color=always --icons=always -a
-    abbr -a -- ll eza --color=always --icons=always -al
-    abbr -a -- lt eza --color=always --icons=always -T
-    abbr -a -- zj zellij
-    set -x EDITOR hx
 end
 
 # opam configuration
 source /home/itt/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
-mise activate fish | source
+
+# mise activate fish | source
+
+abbr -a -- ls eza --color=always --icons=always -a
+abbr -a -- ll eza --color=always --icons=always -al
+abbr -a -- lt eza --color=always --icons=always -T --git-ignore
+abbr -a -- zj zellij
+
+set -x EDITOR hx
+set -x PNPM_HOME $HOME/.local/share/pnpm
+
+fish_add_path $HOME/.dotnet/tools
+fish_add_path $HOME/.moon/bin
+fish_add_path $HOME/.bun/bin
+fish_add_path $PNPM_HOME
+
+starship init fish | source
--- .config/fish/functions/gf.fish
+++ .config/fish/functions/gf.fish
@@ -1,3 +1,4 @@
 function gf
-    set -l repodir (ghq list | fzf) && cd (ghq root)/$repodir
+    # set -l repodir (ghq list | fzf --preview='eza -Ta --git-ignore $(ghq root)/{}') && cd (ghq root)/$repodir
+    set -l repodir (ghq list | tv --preview 'eza -Ta --git-ignore $(ghq root)/{}') && cd (ghq root)/$repodir
 end
